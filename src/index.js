856e3040eb57eca3146b2616da23f71e98b55e4d
Justin Zelinsky <jpzelinsky@gmail.com>
2020-09-10 18:05:08 -0400

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 'use strict';
 
 const minimist = require('minimist');
-const rainyMood = require('./rainymood');
 const packageJson = require('../package.json');
+const path = require('path');
+const playSound = require('play-sound');
 
 const flags = minimist(process.argv.slice(2));
 
@@ -19,6 +20,24 @@ if (flags.help) {
 } else if(flags.version || flags.v) {
   console.log(packageJson.version);
 } else {
-  const { timeout } = flags;
-  rainyMood(timeout);
+  const player = playSound();
+  const rainyMoodAudio = path.join(__dirname, 'assets/RainyMood.m4a');
+
+  console.log(
+    '\x1b[36m%s\x1b[0m',
+    "It's a nice, rainy day! Enjoy. \uD83C\uDF27"
+  );
+
+  const audio = player.play(rainyMoodAudio, function(err) {
+    if (err) {
+      throw err;
+    }
+  });
+
+  if (flags.timeout) {
+    setTimeout(function() {
+      console.log('\x1b[31m%s\x1b[0m', "Sun's out! \uD83C\uDF24");
+      audio.kill();
+    }, timeout * 60 * 1000);
+  }
 }
